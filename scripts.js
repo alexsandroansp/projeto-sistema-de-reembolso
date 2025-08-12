7b305ddb27e4d6b99a3a489867aba39f8d4509e7
Alexsandro Oliveira <alexsandrolt1996@gmail.com>
2025-08-12 18:07:41 -0300

    responsividade

--- scripts.js
+++ scripts.js
@@ -74,6 +74,8 @@ function expenseAdd(newExpense) {
 
     expenseList.append(expenseItem);
 
+    formClear();
+
     updateTotals();
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
@@ -132,3 +134,11 @@ expenseList.addEventListener("click", function (event) {
 
   updateTotals();
 });
+
+function formClear() {
+  expense.value = "";
+  category.value = "";
+  amount.value = "";
+
+  expense.focus();
+}
